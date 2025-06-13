9239f8c203ddb85fecb9ad01c44d8e48770b9fb6
Daniel Cortes <carlosdacario@gmail.com>
2025-06-13 18:33:17 -0500

    13/06/2025

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { Oferta } from './components/oferta/oferta';
 import { Home } from './components/home/home';
+import { Oferta } from './components/oferta/oferta';
+
 
 const routes: Routes = [
   { path: '', component: Home },
-  { path: 'oferta', component: Oferta}
+  { path: 'oferta', component: Oferta},
+  {path: '**', redirectTo: ''}
 ];
 
 @NgModule({
---
+++ src/app/components/footer-alt/footer-alt.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-alt',
+  standalone: false,
+  templateUrl: './footer-alt.html',
+  styleUrl: './footer-alt.css'
+})
+export class FooterAlt {
+
+}
